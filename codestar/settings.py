7c317ef95edd42b852eab368d3a05a4b71116e48
Claudia Schoeman <claudiaschoeman1@gmail.com>
2025-01-01 14:05:14 +0000

    enable serving of image files

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%cxp%&24g&ft&2viqwa9wqqo=patf*g0#@h1hah@1ajt$-gp3!'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
